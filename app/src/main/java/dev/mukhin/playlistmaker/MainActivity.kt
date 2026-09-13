00477f0aff17946e24f4093b9d8cc537ac699f75
Sergey Mukhin <mukhin.dev@gmail.com>
2026-09-13 18:18:12 +0200

    Добавлены кнопки на экран Main и обработка их нажатий

--- app/src/main/java/dev/mukhin/playlistmaker/MainActivity.kt
+++ app/src/main/java/dev/mukhin/playlistmaker/MainActivity.kt
@@ -1,11 +1,38 @@
 package dev.mukhin.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaLibraryButton = findViewById<Button>(R.id.media_library_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        val searchButtonClickListener = object : View.OnClickListener {
+            override fun onClick(v: View) {
+                showToast("Search")
+            }
+        }
+
+        searchButton.setOnClickListener(searchButtonClickListener)
+
+        mediaLibraryButton.setOnClickListener {
+            showToast("Media Library")
+        }
+
+        settingsButton.setOnClickListener {
+            showToast("Settings")
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
     }
 }
